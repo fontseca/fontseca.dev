e7fe8993a5486a6234608faa1a9d8f3cd587a3a6
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-02-06 16:31:31 -0600

    Implement 'MeRepository' interface (#1).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- repository/me_repository.go
+++ repository/me_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
   "context"
+  "database/sql"
   "fontseca/model"
   "fontseca/transfer"
 )
@@ -9,4 +10,22 @@ import (
 type MeRepository interface {
   Get(ctx context.Context) (me model.Me, err error)
   Update(ctx context.Context, update transfer.MeUpdate) (ok bool, err error)
+}
+
+type meRepositoryImpl struct {
+  db *sql.DB
+}
+
+func NewRepository(db *sql.DB) MeRepository {
+  return &meRepositoryImpl{db}
+}
+
+func (r *meRepositoryImpl) Get(ctx context.Context) (me model.Me, err error) {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (r *meRepositoryImpl) Update(ctx context.Context, update transfer.MeUpdate) (ok bool, err error) {
+  // TODO implement me
+  panic("implement me")
 }
