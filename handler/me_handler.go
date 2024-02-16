c68d1a4cd3899dcbc7ff9e9d08212a7437c58089
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-02-16 09:23:12 -0600

    Implement 'SetPhoto' method and unit test it (#1).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- handler/me_handler.go
+++ handler/me_handler.go
@@ -4,8 +4,11 @@ import (
   "errors"
   "fontseca/problem"
   "fontseca/service"
+  "fontseca/transfer"
   "github.com/gin-gonic/gin"
   "net/http"
+  "net/url"
+  "strings"
 )
 
 type MeHandler struct {
@@ -28,4 +31,48 @@ func (h *MeHandler) Get(c *gin.Context) {
     return
   }
   c.JSON(http.StatusOK, me)
+}
+
+func (h *MeHandler) validateURL(w http.ResponseWriter, raw string) (u string, ok bool) {
+  var uri, err = url.ParseRequestURI(raw)
+
+  if nil != err {
+    b := problem.Builder{}
+    b.Title("Unprocessable photo URL format.")
+    b.Status(http.StatusUnprocessableEntity)
+    b.Detail("There was an error parsing the requested URL. Please try with a different URL or verify the current one for correctness.")
+    b.With("photo_url", raw)
+    b.Problem().Emit(w)
+    return "", false
+  }
+
+  return uri.String(), true
+}
+
+func (h *MeHandler) SetPhoto(c *gin.Context) {
+  var photoURL = strings.TrimSpace(c.PostForm("photo_url"))
+  var ok bool
+
+  if "" != photoURL {
+    if photoURL, ok = h.validateURL(c.Writer, photoURL); !ok {
+      return
+    }
+  }
+
+  ok, err := h.s.Update(c, &transfer.MeUpdate{PhotoURL: photoURL})
+  if nil != err {
+    var p problem.Problem
+    if errors.As(err, &p) {
+      p.Emit(c.Writer)
+    } else {
+      problem.NewInternalProblem().Emit(c.Writer)
+    }
+    return
+  }
+
+  if ok {
+    c.Status(http.StatusNoContent)
+  } else {
+    c.Redirect(http.StatusSeeOther, "/me.info")
+  }
 }
--- handler/me_handler_test.go
+++ handler/me_handler_test.go
@@ -3,11 +3,13 @@ package handler
 import (
   "fontseca/mocks"
   "fontseca/model"
+  "fontseca/transfer"
   "github.com/gin-gonic/gin"
   "github.com/stretchr/testify/assert"
   "github.com/stretchr/testify/mock"
   "net/http"
   "net/http/httptest"
+  "strings"
   "testing"
   "time"
 )
@@ -48,5 +50,67 @@ func TestMeHandler_Get(t *testing.T) {
     engine.ServeHTTP(recorder, request)
     assert.Equal(t, http.StatusOK, recorder.Code)
     assert.Equal(t, string(marshal(t, me)), recorder.Body.String())
+  })
+}
+
+func TestMeHandler_SetPhoto(t *testing.T) {
+  const routine = "Update"
+  const method = http.MethodPost
+  const target = "/me.setPhoto"
+
+  t.Run("success", func(t *testing.T) {
+    var urls = []string{
+      "  \t\n\t  ",
+      "https://picsum.photos/200/300",
+      "http://www.picture.com/",
+    }
+
+    for _, url := range urls {
+      url = strings.TrimSpace(url)
+      var expected = transfer.MeUpdate{PhotoURL: url}
+
+      var s = mocks.NewMeService()
+      s.On(routine, mock.AnythingOfType("*gin.Context"), &expected).Return(true, nil)
+
+      var request = httptest.NewRequest(method, target, nil)
+      _ = request.ParseForm()
+      request.PostForm.Set("photo_url", url)
+      gin.SetMode(gin.ReleaseMode)
+      var engine = gin.Default()
+      engine.POST(target, NewMeHandler(s).SetPhoto)
+      var recorder = httptest.NewRecorder()
+      engine.ServeHTTP(recorder, request)
+
+      assert.Equal(t, http.StatusNoContent, recorder.Code)
+      assert.Empty(t, recorder.Body.String())
+      assert.Empty(t, recorder.Header())
+    }
+  })
+
+  t.Run("wrong urls", func(t *testing.T) {
+    var urls = []string{
+      "  \t\n . \n\t  ",
+      "picsum.photos/200/300",
+      "foo/bar/",
+      "gotlim.com",
+    }
+
+    for _, url := range urls {
+      var s = mocks.NewMeService()
+      s.AssertNotCalled(t, routine)
+
+      var request = httptest.NewRequest(method, target, nil)
+      _ = request.ParseForm()
+      request.PostForm.Set("photo_url", url)
+      gin.SetMode(gin.ReleaseMode)
+      var engine = gin.Default()
+      engine.POST(target, NewMeHandler(s).SetPhoto)
+      var recorder = httptest.NewRecorder()
+      engine.ServeHTTP(recorder, request)
+
+      assert.Equal(t, http.StatusUnprocessableEntity, recorder.Code)
+      assert.NotEmpty(t, recorder.Body.String())
+      assert.Contains(t, recorder.Body.String(), "Unprocessable photo URL format.")
+    }
   })
 }
