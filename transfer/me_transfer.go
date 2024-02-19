b11152e3a2c62a87d21cd3d3cd63fc72668994fc
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-02-19 11:52:11 -0600

    Add bindings to 'MeUpdate' type.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- transfer/me_transfer.go
+++ transfer/me_transfer.go
@@ -2,17 +2,17 @@ package transfer
 
 // MeUpdate represents the data structure used for updating my profile information.
 type MeUpdate struct {
-  Summary      string `json:"summary"`
-  JobTitle     string `json:"job_title"`
-  Email        string `json:"email"`
-  PhotoURL     string `json:"photo_url"`
-  ResumeURL    string `json:"resume_url"`
-  Company      string `json:"company"`
-  Location     string `json:"location"`
-  Hireable     bool   `json:"hireable"`
-  GitHubURL    string `json:"github_url"`
-  LinkedInURL  string `json:"linkedin_url"`
-  YouTubeURL   string `json:"youtube_url"`
-  TwitterURL   string `json:"twitter_url"`
-  InstagramURL string `json:"instagram_url"`
+  Summary      string `json:"summary" binding:"max=1024"`
+  JobTitle     string `json:"job_title" binding:"max=64"`
+  Email        string `json:"email" binding:"max=254"`
+  PhotoURL     string `json:"-"`
+  ResumeURL    string `json:"-"`
+  Company      string `json:"company" binding:"max=64"`
+  Location     string `json:"location" binding:"max=64"`
+  Hireable     bool   `json:"-"`
+  GitHubURL    string `json:"github_url" binding:"max=2048"`
+  LinkedInURL  string `json:"linkedin_url" binding:"max=2048"`
+  YouTubeURL   string `json:"youtube_url" binding:"max=2048"`
+  TwitterURL   string `json:"twitter_url" binding:"max=2048"`
+  InstagramURL string `json:"instagram_url" binding:"max=2048"`
 }
