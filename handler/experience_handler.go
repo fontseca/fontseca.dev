e6a7ba33050322f5fab996b5c22bea7e4043949e
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-02-27 08:42:13 -0600

    Implement 'Get' handler method (#6).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- handler/experience_handler.go
+++ handler/experience_handler.go
@@ -1,7 +1,11 @@
 package handler
 
 import (
+  "errors"
+  "fontseca/problem"
   "fontseca/service"
+  "github.com/gin-gonic/gin"
+  "net/http"
 )
 
 type ExperienceHandler struct {
@@ -10,4 +14,18 @@ type ExperienceHandler struct {
 
 func NewExperienceHandler(s service.ExperienceService) *ExperienceHandler {
   return &ExperienceHandler{s}
+}
+
+func (h *ExperienceHandler) Get(c *gin.Context) {
+  var e, err = h.s.Get(c)
+  if nil != err {
+    var p *problem.Problem
+    if errors.As(err, &p) {
+      p.Emit(c.Writer)
+    } else {
+      problem.NewInternal().Emit(c.Writer)
+    }
+    return
+  }
+  c.JSON(http.StatusOK, e)
 }
---
+++ handler/experience_handler_test.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+  "fontseca/mocks"
+  "fontseca/model"
+  "github.com/gin-gonic/gin"
+  "github.com/stretchr/testify/assert"
+  "github.com/stretchr/testify/mock"
+  "net/http"
+  "net/http/httptest"
+  "testing"
+)
+
+func TestExperienceHandler_Get(t *testing.T) {
+  const routine = "Get"
+  const method = http.MethodGet
+  const target = "/experience.list"
+
+  t.Run("success", func(t *testing.T) {
+    var e = make([]*model.Experience, 0)
+    var s = mocks.NewExperienceService()
+    s.On(routine, mock.AnythingOfType("*gin.Context"), []bool(nil)).Return(e, nil)
+    gin.SetMode(gin.ReleaseMode)
+    var engine = gin.Default()
+    engine.GET(target, NewExperienceHandler(s).Get)
+    var request = httptest.NewRequest(method, target, nil)
+    var recorder = httptest.NewRecorder()
+    engine.ServeHTTP(recorder, request)
+    assert.Equal(t, http.StatusOK, recorder.Code)
+    assert.Equal(t, string(marshal(t, e)), recorder.Body.String())
+  })
+}
