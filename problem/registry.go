c6f5d5edacf3ce50874ed16f496c919c033f5d4e
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-02-29 18:24:47 -0600

    Add to problem registry: 'NewUnparsableValue' and 'NewValueOutOfRange'.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- problem/registry.go
+++ problem/registry.go
@@ -25,3 +25,27 @@ func NewNotFound(id, recordType string) *Problem {
   p.With("record_type", recordType)
   return &p
 }
+
+func NewUnparsableValue(targetType, fieldName, fieldValue string) *Problem {
+  var p Problem
+  p.Type("about:blank")
+  p.Status(http.StatusUnprocessableEntity)
+  p.Title(fmt.Sprintf("Failure when parsing %s value.", targetType))
+  p.Detail(fmt.Sprintf("Failed to parse the provided value as: %s. Please make sure the value is valid according to its type.", targetType))
+  p.With("field_name", fieldName)
+  p.With("field_type", targetType)
+  p.With("field_value", fieldValue)
+  return &p
+}
+
+func NewValueOutOfRange(targetType, fieldName, fieldValue string) *Problem {
+  var p Problem
+  p.Type("about:blank")
+  p.Status(http.StatusUnprocessableEntity)
+  p.Title(fmt.Sprintf("Failure when parsing %s value.", targetType))
+  p.Detail(fmt.Sprintf("Out of range for the provided value as: %s. Please make sure the value is valid according to its type.", targetType))
+  p.With("field_name", fieldName)
+  p.With("field_type", targetType)
+  p.With("field_value", fieldValue)
+  return &p
+}
