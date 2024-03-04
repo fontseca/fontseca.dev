cf482c63d6b6fcacb6b1bdbe6c0d5be2b367a570
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-03-04 11:47:48 -0600

    Change bindings.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- transfer/experience_transfer.go
+++ transfer/experience_transfer.go
@@ -2,8 +2,8 @@ package transfer
 
 // ExperienceCreation represents the data required to create a new experience entry.
 type ExperienceCreation struct {
-  Starts   int    `json:"starts" binding:"required,number,gt=2027"`
-  Ends     int    `json:"ends" binding:"number,gt=2027"`
+  Starts   int    `json:"starts" binding:"required,number,gt=2017"`
+  Ends     int    `json:"ends" binding:"number"`
   JobTitle string `json:"job_title" binding:"required,max=64"`
   Company  string `json:"company" binding:"required,max=64"`
   Country  string `json:"country" binding:"required,max=64"`
@@ -12,8 +12,8 @@ type ExperienceCreation struct {
 
 // ExperienceUpdate represents the data required to update an existing experience entry.
 type ExperienceUpdate struct {
-  Starts   int    `json:"starts" binding:"number,gt=2027"`
-  Ends     int    `json:"ends" binding:"number,gt=2027"`
+  Starts   int    `json:"starts" binding:"number"`
+  Ends     int    `json:"ends" binding:"number"`
   JobTitle string `json:"job_title" binding:"max=64"`
   Company  string `json:"company" binding:"max=64"`
   Country  string `json:"country" binding:"max=64"`
