d9a6e03eb84488119ecfae46941d51c9d36788e1
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-03-05 10:00:24 -0600

    Add 'ProjectUpdate' transfer type (#9).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- transfer/project_transfer.go
+++ transfer/project_transfer.go
@@ -17,3 +17,21 @@ type ProjectCreation struct {
   Archived       bool   `json:"archived"`
   Finished       bool   `json:"finished"`
 }
+
+// ProjectUpdate represents the data required to update an existing project entry.
+type ProjectUpdate struct {
+  Name           string `json:"name"`
+  Homepage       string `json:"homepage"`
+  Language       string `json:"language"`
+  Summary        string `json:"summary"`
+  Content        string `json:"content"`
+  EstimatedTime  int    `json:"estimated_time"`
+  FirstImageURL  string `json:"first_image_url"`
+  SecondImageURL string `json:"second_image_url"`
+  GitHubURL      string `json:"github_url"`
+  CollectionURL  string `json:"collection_url"`
+  PlaygroundURL  string `json:"playground_url"`
+  Playable       bool   `json:"playable"`
+  Archived       bool   `json:"archived"`
+  Finished       bool   `json:"finished"`
+}
