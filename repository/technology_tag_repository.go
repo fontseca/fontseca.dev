4e250bc85fead34ec7cb5c7f151d532a5d0a54db
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-03-06 11:37:36 -0600

    Add 'TechnologyTagRepository' repository interface (#9).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

---
+++ repository/technology_tag_repository.go
@@ -0,0 +1,22 @@
+package repository
+
+import (
+  "context"
+  "fontseca/model"
+  "fontseca/transfer"
+)
+
+// TechnologyTagRepository provides methods for interacting with technology tags data in the database.
+type TechnologyTagRepository interface {
+  // Get retrieves a slice of technology tags.
+  Get(ctx context.Context) (technologies []*model.TechnologyTag, err error)
+
+  // Add creates a new technology tag record with the provided creation data.
+  Add(ctx context.Context, creation *transfer.TechnologyTagCreation) (id string, err error)
+
+  // Update modifies an existing technology tag record with the provided update data.
+  Update(ctx context.Context, id string, update *transfer.TechnologyTagUpdate) (updated bool, err error)
+
+  // Remove deletes an existing technology tag. If not found, returns a not found error.
+  Remove(ctx context.Context, id string) (err error)
+}
