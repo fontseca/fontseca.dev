324522b0f8b2d2851f9efdc0498de9aa084c5afd
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-03-06 17:52:43 -0600

    Add 'TechnologyTagService' mock type (#9).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- mocks/technology_tag_mocks.go
+++ mocks/technology_tag_mocks.go
@@ -39,3 +39,36 @@ func (o *TechnologyTagRepository) Remove(ctx context.Context, id string) (err er
   var args = o.Called(ctx, id)
   return args.Error(0)
 }
+
+type TechnologyTagService struct {
+  mock.Mock
+}
+
+func (o *TechnologyTagService) Get(ctx context.Context) (technologies []*model.TechnologyTag, err error) {
+  var args = o.Called(ctx)
+  var arg0 = args.Get(0)
+  if nil != arg0 {
+    technologies = arg0.([]*model.TechnologyTag)
+  }
+  return technologies, args.Error(1)
+}
+
+func (o *TechnologyTagService) Add(ctx context.Context, creation *transfer.TechnologyTagCreation) (id string, err error) {
+  var args = o.Called(ctx, id, creation)
+  return args.String(0), args.Error(1)
+}
+
+func (o *TechnologyTagService) Exists(ctx context.Context, id string) (err error) {
+  var args = o.Called(ctx, id)
+  return args.Error(0)
+}
+
+func (o *TechnologyTagService) Update(ctx context.Context, id string, update *transfer.TechnologyTagUpdate) (updated bool, err error) {
+  var args = o.Called(ctx, id, update)
+  return args.Bool(0), args.Error(1)
+}
+
+func (o *TechnologyTagService) Remove(ctx context.Context, id string) (err error) {
+  var args = o.Called(ctx, id)
+  return args.Error(0)
+}
