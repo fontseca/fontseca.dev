cdf3d7347ba315ea4d506f2f78e4de0952f82a06
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-03-06 17:45:10 -0600

    Implement 'TechnologyTagService' interface (#9).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- service/technology_tag_service.go
+++ service/technology_tag_service.go
@@ -3,6 +3,7 @@ package service
 import (
   "context"
   "fontseca/model"
+  "fontseca/repository"
   "fontseca/transfer"
 )
 
@@ -24,4 +25,37 @@ type TechnologyTagService interface {
 
   // Remove deletes an existing technology tag. If not found, returns a not found error.
   Remove(ctx context.Context, id string) (err error)
+}
+
+type technologyTagService struct {
+  r repository.TechnologyTagRepository
+}
+
+func NewTechnologyTagService(repository repository.TechnologyTagRepository) TechnologyTagService {
+  return &technologyTagService{repository}
+}
+
+func (s *technologyTagService) Get(ctx context.Context) (technologies []*model.TechnologyTag, err error) {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *technologyTagService) Add(ctx context.Context, creation *transfer.TechnologyTagCreation) (id string, err error) {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *technologyTagService) Exists(ctx context.Context, id string) (err error) {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *technologyTagService) Update(ctx context.Context, id string, update *transfer.TechnologyTagUpdate) (updated bool, err error) {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *technologyTagService) Remove(ctx context.Context, id string) (err error) {
+  // TODO implement me
+  panic("implement me")
 }
