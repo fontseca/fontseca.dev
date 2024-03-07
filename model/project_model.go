e6e914077d8a20394c233ec2cabe2d64406067b3
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-03-07 14:06:24 -0600

    Add 'TechnologyTags' field to 'Project' model (#9).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- model/project_model.go
+++ model/project_model.go
@@ -7,21 +7,22 @@ import (
 
 // Project represents a project that is being developed by me.
 type Project struct {
-  ID             uuid.UUID `json:"id"`
-  Name           string    `json:"name"`
-  Homepage       string    `json:"homepage"`
-  Language       string    `json:"language"`
-  Summary        string    `json:"summary"`
-  Content        string    `json:"content"`
-  EstimatedTime  int       `json:"estimated_time"`
-  FirstImageURL  string    `json:"first_image_url"`
-  SecondImageURL string    `json:"second_image_url"`
-  GitHubURL      string    `json:"github_url"`
-  CollectionURL  string    `json:"collection_url"`
-  PlaygroundURL  string    `json:"playground_url"`
-  Playable       bool      `json:"playable"`
-  Archived       bool      `json:"archived"`
-  Finished       bool      `json:"finished"`
-  CreatedAt      time.Time `json:"created_at"`
-  UpdatedAt      time.Time `json:"updated_at"`
+  ID             uuid.UUID        `json:"id"`
+  Name           string           `json:"name"`
+  Homepage       string           `json:"homepage"`
+  Language       string           `json:"language"`
+  Summary        string           `json:"summary"`
+  Content        string           `json:"content"`
+  EstimatedTime  int              `json:"estimated_time"`
+  FirstImageURL  string           `json:"first_image_url"`
+  SecondImageURL string           `json:"second_image_url"`
+  GitHubURL      string           `json:"github_url"`
+  CollectionURL  string           `json:"collection_url"`
+  PlaygroundURL  string           `json:"playground_url"`
+  Playable       bool             `json:"playable"`
+  Archived       bool             `json:"archived"`
+  Finished       bool             `json:"finished"`
+  TechnologyTags []*TechnologyTag `json:"technology_tags"`
+  CreatedAt      time.Time        `json:"created_at"`
+  UpdatedAt      time.Time        `json:"updated_at"`
 }
