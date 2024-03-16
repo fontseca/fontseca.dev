79a410bb437d0c62ea67ff46c34ab9bf28f13372
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-03-16 14:59:56 -0600

    Add 'Add' handler method (#9).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- handler/projects_handler.go
+++ handler/projects_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
   "fontseca/service"
+  "fontseca/transfer"
   "github.com/gin-gonic/gin"
   "net/http"
 )
@@ -39,4 +40,19 @@ func (h *ProjectsHandler) GetByID(c *gin.Context) {
     return
   }
   c.JSON(http.StatusOK, project)
+}
+
+func (h *ProjectsHandler) Add(c *gin.Context) {
+  var creation = transfer.ProjectCreation{}
+  if err := bindPostForm(c, &creation); check(err, c.Writer) {
+    return
+  }
+  if err := validateStruct(&creation); check(err, c.Writer) {
+    return
+  }
+  var insertedID, err = h.s.Add(c, &creation)
+  if check(err, c.Writer) {
+    return
+  }
+  c.JSON(http.StatusOK, gin.H{"inserted_id": insertedID})
 }
--- handler/projects_handler_test.go
+++ handler/projects_handler_test.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+  "errors"
   "fontseca/mocks"
   "fontseca/model"
+  "fontseca/problem"
+  "fontseca/transfer"
   "github.com/gin-gonic/gin"
   "github.com/google/uuid"
   "github.com/stretchr/testify/assert"
@@ -93,5 +96,73 @@ func TestProjectsHandler_GetByID(t *testing.T) {
     engine.ServeHTTP(recorder, request)
     assert.Equal(t, http.StatusOK, recorder.Code)
     assert.Equal(t, string(marshal(t, project)), recorder.Body.String())
+  })
+}
+
+func TestProjectsHandler_Add(t *testing.T) {
+  const routine = "Add"
+  const method = http.MethodPost
+  const target = "/me.projects.add"
+  var id = uuid.New().String()
+  var creation = &transfer.ProjectCreation{
+    Name:           "Name",
+    Homepage:       "https://Homepage.com",
+    Language:       "Go",
+    Summary:        "Summary.",
+    Content:        "Content.",
+    EstimatedTime:  1,
+    FirstImageURL:  "https://FirstImageURL.com",
+    SecondImageURL: "https://SecondImageURL.com",
+    GitHubURL:      "https://GitHubURL.com",
+    CollectionURL:  "https://CollectionURL.com",
+  }
+  var request = httptest.NewRequest(method, target, nil)
+  _ = request.ParseForm()
+  request.PostForm.Add("name", creation.Name)
+  request.PostForm.Add("homepage", creation.Homepage)
+  request.PostForm.Add("language", creation.Language)
+  request.PostForm.Add("summary", creation.Summary)
+  request.PostForm.Add("content", creation.Content)
+  request.PostForm.Add("estimated_time", "1")
+  request.PostForm.Add("first_image_url", creation.FirstImageURL)
+  request.PostForm.Add("second_image_url", creation.SecondImageURL)
+  request.PostForm.Add("github_url", creation.GitHubURL)
+  request.PostForm.Add("collection_url", creation.CollectionURL)
+
+  t.Run("success", func(t *testing.T) {
+    var s = mocks.NewProjectsService()
+    s.On(routine, mock.AnythingOfType("*gin.Context"), creation).Return(id, nil)
+    var engine = gin.Default()
+    engine.POST(target, NewProjectsHandler(s).Add)
+    var recorder = httptest.NewRecorder()
+    engine.ServeHTTP(recorder, request)
+    assert.Equal(t, http.StatusOK, recorder.Code)
+    assert.Equal(t, string(marshal(t, gin.H{"inserted_id": id})), recorder.Body.String())
+  })
+
+  t.Run("expected problem detail", func(t *testing.T) {
+    var expected = &problem.Problem{}
+    expected.Status(http.StatusGone)
+    expected.Detail("Expected problem detail.")
+    var s = mocks.NewProjectsService()
+    s.On(routine, mock.AnythingOfType("*gin.Context"), creation).Return("", expected)
+    var engine = gin.Default()
+    engine.POST(target, NewProjectsHandler(s).Add)
+    var recorder = httptest.NewRecorder()
+    engine.ServeHTTP(recorder, request)
+    assert.Equal(t, http.StatusGone, recorder.Code)
+    assert.Contains(t, recorder.Body.String(), "Expected problem detail.")
+  })
+
+  t.Run("unexpected error", func(t *testing.T) {
+    var unexpected = errors.New("unexpected error")
+    var s = mocks.NewProjectsService()
+    s.On(routine, mock.AnythingOfType("*gin.Context"), creation).Return("", unexpected)
+    var engine = gin.Default()
+    engine.POST(target, NewProjectsHandler(s).Add)
+    var recorder = httptest.NewRecorder()
+    engine.ServeHTTP(recorder, request)
+    assert.Equal(t, http.StatusInternalServerError, recorder.Code)
+    assert.Contains(t, recorder.Body.String(), "An unexpected error occurred while processing your request")
   })
 }
