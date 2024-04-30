5229329445750e4a0ca3dae551814437c1e67c6a
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-04-30 08:38:11 -0600

    Add 'TopicUpdate' struct type (#17).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- transfer/archive_transfer.go
+++ transfer/archive_transfer.go
@@ -30,3 +30,8 @@ type ArticleUpdate struct {
 type TopicCreation struct {
   Name string `json:"name" binding:"required,max=64"`
 }
+
+// TopicUpdate represents the data required to update an existing topic entry.
+type TopicUpdate struct {
+  Name string `json:"name" binding:"required,max=64"`
+}
