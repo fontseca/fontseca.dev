26a9f3788e8646d983e5e3cdc5234204575a3788
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-04-30 08:27:23 -0600

    Add 'ArticleUpdate' struct type (#17).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- transfer/archive_transfer.go
+++ transfer/archive_transfer.go
@@ -12,3 +12,16 @@ type ArticleCreation struct {
   Content   string `json:"content"`
   DraftedAt time.Time
 }
+
+// ArticleUpdate represents the data required to update an existing article entry.
+type ArticleUpdate struct {
+  Title       string `json:"title"`
+  Slug        string
+  ReadTime    int
+  Content     string `json:"content"`
+  DraftedAt   time.Time
+  PinnedAt    *time.Time
+  ArchivedAt  *time.Time
+  PublishedAt *time.Time
+  ModifiedAt  *time.Time
+}
