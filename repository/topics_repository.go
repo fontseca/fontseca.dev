e52203c6e0c89251f976dc8462b4a00e4c571b86
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-05-18 14:33:32 -0600

    Implement 'Add' repository method (#17).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- repository/topics_repository.go
+++ repository/topics_repository.go
@@ -5,6 +5,9 @@ import (
   "database/sql"
   "fontseca.dev/model"
   "fontseca.dev/transfer"
+  "github.com/google/uuid"
+  "log/slog"
+  "time"
 )
 
 // TopicsRepository is a low level API that provides methods for interacting
@@ -32,8 +35,37 @@ func NewTopicsRepository(db *sql.DB) TopicsRepository {
 }
 
 func (r *topicsRepository) Add(ctx context.Context, creation *transfer.TopicCreation) (insertedUUID string, err error) {
-  // TODO implement me
-  panic("implement me")
+  tx, err := r.db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
+  if nil != err {
+    return uuid.Nil.String(), err
+  }
+
+  defer tx.Rollback()
+
+  addTopicQuery := `
+  INSERT INTO "topic" (name)
+               VALUES (@name)
+    RETURNING "uuid";`
+
+  ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+  defer cancel()
+
+  result := tx.QueryRowContext(ctx, addTopicQuery,
+    sql.Named("name", creation.Name),
+  )
+
+  err = result.Scan(&insertedUUID)
+  if nil != err {
+    slog.Error(err.Error())
+    return uuid.Nil.String(), err
+  }
+
+  if err = tx.Commit(); nil != err {
+    slog.Error(err.Error())
+    return uuid.Nil.String(), err
+  }
+
+  return insertedUUID, nil
 }
 
 func (r *topicsRepository) Get(ctx context.Context) (topics []*model.Topic, err error) {
