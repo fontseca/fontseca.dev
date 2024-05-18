cdda32b669e88596c8a1ed09b0f73385dbcd9d3e
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-05-18 14:23:24 -0600

    Add 'TopicsRepository' repository type (#17).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

---
+++ repository/topics_repository.go
@@ -0,0 +1,23 @@
+package repository
+
+import (
+  "context"
+  "fontseca.dev/model"
+  "fontseca.dev/transfer"
+)
+
+// TopicsRepository is a low level API that provides methods for interacting
+// with topics in the database.
+type TopicsRepository interface {
+  // Add adds a new topic.
+  Add(ctx context.Context, creation *transfer.TopicCreation) (insertedUUID string, err error)
+
+  // Get retrieves all the topics.
+  Get(ctx context.Context) (topics []*model.Topic, err error)
+
+  // Update updates an existing topic.
+  Update(ctx context.Context, id string, update *transfer.TopicUpdate) error
+
+  // Remove removes a topic and detaches it from any article that uses it.
+  Remove(ctx context.Context, id string) error
+}
