3c065dd6931a4cbb9680d71fa15e8b258640624d
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-05-21 17:18:21 -0600

    Implement 'ArticlesService' interface (#17).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- service/articles_service.go
+++ service/articles_service.go
@@ -3,6 +3,7 @@ package service
 import (
   "context"
   "fontseca.dev/model"
+  "fontseca.dev/repository"
 )
 
 // ArticlesService is a high level provider for articles.
@@ -45,8 +46,66 @@ type ArticlesService interface {
   // exists, it returns an error informing about a conflicting state.
   AddTopic(ctx context.Context, articleUUID, topicUUID string) error
 
-  // RemoveTopic removes a topic from article draft. If the article
+  // RemoveTopic removes a topic from article. If the article
   // has no topic identified by its UUID, it returns an error indication
   // a not found state.
   RemoveTopic(ctx context.Context, articleUUID, topicUUID string) error
+}
+
+type articlesService struct {
+  r repository.ArchiveRepository
+}
+
+func NewArticlesService(r repository.ArchiveRepository) ArticlesService {
+  return &articlesService{r}
+}
+
+func (s *articlesService) Get(ctx context.Context, needle string) (articles []*model.Article, err error) {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *articlesService) GetByID(ctx context.Context, articleUUID string) (article *model.Article, err error) {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *articlesService) Hide(ctx context.Context, id string) error {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *articlesService) Show(ctx context.Context, id string) error {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *articlesService) Amend(ctx context.Context, id string) error {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *articlesService) Remove(ctx context.Context, id string) error {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *articlesService) Pin(ctx context.Context, id string) error {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *articlesService) Unpin(ctx context.Context, id string) error {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *articlesService) AddTopic(ctx context.Context, articleUUID, topicUUID string) error {
+  // TODO implement me
+  panic("implement me")
+}
+
+func (s *articlesService) RemoveTopic(ctx context.Context, articleUUID, topicUUID string) error {
+  // TODO implement me
+  panic("implement me")
 }
