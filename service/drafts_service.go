92c6642bb1a9ed9557bed06ba3c8a84cb777d866
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-05-21 08:34:56 -0600

    Refactor drafts service.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- service/drafts_service.go
+++ service/drafts_service.go
@@ -5,6 +5,7 @@ import (
   "fontseca.dev/model"
   "fontseca.dev/repository"
   "fontseca.dev/transfer"
+  "github.com/google/uuid"
 )
 
 // DraftsService is a high level provider for article drafts.
@@ -14,12 +15,12 @@ type DraftsService interface {
   //
   // To draft an article, only its title is required, other fields
   // are completely optional and can be added in an eventual revision.
-  Draft(ctx context.Context, creation *transfer.ArticleCreation) (id string, err error)
+  Draft(ctx context.Context, creation *transfer.ArticleCreation) (insertedUUID uuid.UUID, err error)
 
   // Publish makes a draft publicly available.
   //
   // Invoking Publish on an already published article has no effect.
-  Publish(ctx context.Context, id string) error
+  Publish(ctx context.Context, draftUUID string) error
 
   // Get retrieves all the ongoing articles drafts.
   //
@@ -30,30 +31,30 @@ type DraftsService interface {
   Get(ctx context.Context, needle string) (articles []*model.Article, err error)
 
   // GetByID retrieves one article draft by its UUID.
-  GetByID(ctx context.Context, id string) (article *model.Article, err error)
+  GetByID(ctx context.Context, draftUUID string) (article *model.Article, err error)
 
   // AddTopic adds a topic to the article draft. If the topic already
   // exists, it returns an error informing about a conflicting state.
-  AddTopic(ctx context.Context, articleID, topicID string) error
+  AddTopic(ctx context.Context, draftUUID, topicUUID string) error
 
   // RemoveTopic removes a topic from the article draft. If the article
   // has  no topic identified by its UUID, it returns an error indication
   // a not found state.
-  RemoveTopic(ctx context.Context, articleID, topicID string) error
+  RemoveTopic(ctx context.Context, draftUUID, topicUUID string) error
 
   // Share creates a shareable link for an article draft. Only users
   // with that link can see the progress and provide feedback.
   //
   // A shareable link does not make an article public. This link will
   // eventually expire after a certain amount of time.
-  Share(ctx context.Context, id string) (link string, err error)
+  Share(ctx context.Context, draftUUID string) (link string, err error)
 
   // Discard completely drops an article draft.
-  Discard(ctx context.Context, id string) error
+  Discard(ctx context.Context, draftUUID string) error
 
   // Revise adds a correction or inclusion to an article draft in order
   // to correct or improve it.
-  Revise(ctx context.Context, id string, revision *transfer.ArticleUpdate) error
+  Revise(ctx context.Context, draftUUID string, revision *transfer.ArticleUpdate) error
 }
 
 type draftsService struct {
@@ -64,12 +65,12 @@ func NewDraftsService(r repository.ArchiveRepository) DraftsService {
   return &draftsService{r}
 }
 
-func (s *draftsService) Draft(ctx context.Context, creation *transfer.ArticleCreation) (id string, err error) {
+func (s *draftsService) Draft(ctx context.Context, creation *transfer.ArticleCreation) (insertedUUID uuid.UUID, err error) {
   // TODO implement me
   panic("implement me")
 }
 
-func (s *draftsService) Publish(ctx context.Context, id string) error {
+func (s *draftsService) Publish(ctx context.Context, draftUUID string) error {
   // TODO implement me
   panic("implement me")
 }
@@ -79,32 +80,32 @@ func (s *draftsService) Get(ctx context.Context, needle string) (articles []*mod
   panic("implement me")
 }
 
-func (s *draftsService) GetByID(ctx context.Context, id string) (article *model.Article, err error) {
+func (s *draftsService) GetByID(ctx context.Context, draftUUID string) (article *model.Article, err error) {
   // TODO implement me
   panic("implement me")
 }
 
-func (s *draftsService) AddTopic(ctx context.Context, articleID, topicID string) error {
+func (s *draftsService) AddTopic(ctx context.Context, draftUUID, topicUUID string) error {
   // TODO implement me
   panic("implement me")
 }
 
-func (s *draftsService) RemoveTopic(ctx context.Context, articleID, topicID string) error {
+func (s *draftsService) RemoveTopic(ctx context.Context, draftUUID, topicUUID string) error {
   // TODO implement me
   panic("implement me")
 }
 
-func (s *draftsService) Share(ctx context.Context, id string) (link string, err error) {
+func (s *draftsService) Share(ctx context.Context, draftUUID string) (link string, err error) {
   // TODO implement me
   panic("implement me")
 }
 
-func (s *draftsService) Discard(ctx context.Context, id string) error {
+func (s *draftsService) Discard(ctx context.Context, draftUUID string) error {
   // TODO implement me
   panic("implement me")
 }
 
-func (s *draftsService) Revise(ctx context.Context, id string, revision *transfer.ArticleUpdate) error {
+func (s *draftsService) Revise(ctx context.Context, draftUUID string, revision *transfer.ArticleUpdate) error {
   // TODO implement me
   panic("implement me")
 }
