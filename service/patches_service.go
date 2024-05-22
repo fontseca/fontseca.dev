32ac2e327afa64293fdf76325750bb553272bdf3
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-05-22 13:53:47 -0600

    Implement 'Revise' method for 'PatchesService' type (#17).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- service/patches_service.go
+++ service/patches_service.go
@@ -2,9 +2,13 @@ package service
 
 import (
   "context"
+  "errors"
   "fontseca.dev/model"
+  "fontseca.dev/problem"
   "fontseca.dev/repository"
   "fontseca.dev/transfer"
+  "log/slog"
+  "strings"
 )
 
 // PatchesService is a high level provider for article patches.
@@ -45,8 +49,51 @@ func (s *patchesService) Get(ctx context.Context) (patches []*model.ArticlePatch
 }
 
 func (s *patchesService) Revise(ctx context.Context, id string, revision *transfer.ArticleUpdate) error {
-  // TODO implement me
-  panic("implement me")
+  if nil == revision {
+    err := errors.New("nil value for parameter: revision")
+    slog.Error(err.Error())
+    return err
+  }
+
+  if err := validateUUID(&id); nil != err {
+    return err
+  }
+
+  revision.Title = strings.TrimSpace(revision.Title)
+  revision.Content = strings.TrimSpace(revision.Content)
+
+  if "" != revision.Title {
+    sanitizeTextWordIntersections(&revision.Title)
+  }
+
+  switch {
+  case 0 != len(revision.Title) && 256 < len(revision.Title):
+    return problem.NewValidation([3]string{"title", "max", "256"})
+  case 0 != len(revision.Content) && 3145728 < len(revision.Content):
+    return problem.NewValidation([3]string{"content", "max", "3145728"})
+  }
+
+  if "" != revision.Title || "" != revision.Content {
+    builder := strings.Builder{}
+
+    if "" != revision.Title {
+      builder.WriteString(revision.Title)
+      revision.Slug = generateSlug(revision.Title)
+    }
+
+    if "" != revision.Content {
+      if 0 < builder.Len() {
+        builder.WriteRune('\n')
+      }
+
+      builder.WriteString(revision.Content)
+    }
+
+    r := strings.NewReader(builder.String())
+    revision.ReadTime = computePostReadingTimeInMinutes(r)
+  }
+
+  return s.r.Revise(ctx, id, revision)
 }
 
 func (s *patchesService) Share(ctx context.Context, id string) (link string, err error) {
--- service/patches_service_test.go
+++ service/patches_service_test.go
@@ -5,7 +5,11 @@ import (
   "errors"
   "fontseca.dev/mocks"
   "fontseca.dev/model"
+  "fontseca.dev/transfer"
+  "github.com/google/uuid"
   "github.com/stretchr/testify/assert"
+  "github.com/stretchr/testify/mock"
+  "strings"
   "testing"
 )
 
@@ -36,5 +40,84 @@ func TestPatchesService_Get(t *testing.T) {
 
     assert.Nil(t, articles)
     assert.ErrorIs(t, err, unexpected)
+  })
+}
+
+func TestPatchesService_Revise(t *testing.T) {
+  const routine = "Revise"
+  ctx := context.TODO()
+  id := uuid.NewString()
+
+  t.Run("success", func(t *testing.T) {
+    revision := &transfer.ArticleUpdate{
+      Title:    "Consectetur! Adipiscing... Quis nostrud: ELIT?",
+      Slug:     "consectetur-adipiscing-quis-nostrud-elit",
+      ReadTime: 11,
+      Content:  strings.Repeat("word ", 1999) + "word",
+    }
+
+    dirty := &transfer.ArticleUpdate{
+      Title:   " \t\n " + revision.Title + " \t\n ",
+      Content: " \t\n " + revision.Content + " \t\n ",
+    }
+
+    r := mocks.NewArchiveRepository()
+    r.On(routine, ctx, id, revision).Return(nil)
+
+    assert.NoError(t, NewPatchesService(r).Revise(ctx, id, dirty))
+  })
+
+  t.Run("success: changing title", func(t *testing.T) {
+    revision := &transfer.ArticleUpdate{
+      Title:    "Consectetur-Adipiscing!!... Quis nostrud: ELIT??? +-'\"",
+      Slug:     "consectetur-adipiscing-quis-nostrud-elit",
+      ReadTime: 1,
+    }
+
+    dirty := &transfer.ArticleUpdate{
+      Title: " \t\n " + revision.Title + " \t\n ",
+    }
+
+    r := mocks.NewArchiveRepository()
+    r.On(routine, mock.Anything, mock.Anything, revision).Return(nil)
+
+    assert.NoError(t, NewPatchesService(r).Revise(ctx, id, dirty))
+  })
+
+  t.Run("success: changing content", func(t *testing.T) {
+    revision := &transfer.ArticleUpdate{
+      Content:  strings.Repeat("word ", 299) + "word",
+      ReadTime: 2,
+    }
+
+    dirty := &transfer.ArticleUpdate{
+      Content: " \t\n " + revision.Content + " \t\n ",
+    }
+
+    r := mocks.NewArchiveRepository()
+    r.On(routine, mock.Anything, mock.Anything, revision).Return(nil)
+
+    assert.NoError(t, NewPatchesService(r).Revise(ctx, id, dirty))
+  })
+
+  t.Run("nil parameter: revision", func(t *testing.T) {
+    r := mocks.NewArchiveRepository()
+    r.AssertNotCalled(t, routine)
+    assert.ErrorContains(t, NewPatchesService(r).Revise(ctx, id, nil), "nil value")
+  })
+
+  t.Run("wrong uuid: id", func(t *testing.T) {
+    r := mocks.NewArchiveRepository()
+    r.AssertNotCalled(t, routine)
+    assert.Error(t, NewPatchesService(r).Revise(ctx, "x", &transfer.ArticleUpdate{}))
+  })
+
+  t.Run("gets a repository failure", func(t *testing.T) {
+    unexpected := errors.New("unexpected error")
+
+    r := mocks.NewArchiveRepository()
+    r.On(routine, mock.Anything, mock.Anything, mock.Anything).Return(unexpected)
+
+    assert.ErrorIs(t, NewPatchesService(r).Revise(ctx, id, &transfer.ArticleUpdate{}), unexpected)
   })
 }
