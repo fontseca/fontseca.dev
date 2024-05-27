c56e31d77989b615ee546072f52ec8b488cccd95
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-05-27 07:44:31 -0600

    Add unit test for 'Hide' method (#17).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- handler/articles_handler_test.go
+++ handler/articles_handler_test.go
@@ -284,3 +284,81 @@ func TestArticlesHandler_GetByID(t *testing.T) {
     assert.Contains(t, recorder.Result().Header.Get("Content-Type"), "application/problem+json")
   })
 }
+
+func TestArticlesHandler_Hide(t *testing.T) {
+  const (
+    routine = "Hide"
+    method  = http.MethodPost
+    target  = "/archive.articles.hide"
+  )
+
+  request := httptest.NewRequest(method, target, nil)
+  id := uuid.NewString()
+
+  _ = request.ParseForm()
+
+  request.PostForm.Add("article_uuid", id)
+
+  t.Run("success", func(t *testing.T) {
+    expectedStatusCode := http.StatusNoContent
+
+    s := mocks.NewArticlesService()
+    s.On(routine, mock.AnythingOfType("*gin.Context"), id).Return(nil)
+
+    engine := gin.Default()
+    engine.POST(target, NewArticlesHandler(s).Hide)
+
+    recorder := httptest.NewRecorder()
+
+    engine.ServeHTTP(recorder, request)
+
+    assert.Equal(t, expectedStatusCode, recorder.Code)
+    assert.Empty(t, recorder.Body)
+    assert.Empty(t, recorder.Result().Cookies())
+  })
+
+  t.Run("expected problem detail", func(t *testing.T) {
+    expectedStatusCode := http.StatusBadRequest
+    expectBodyContains := "Expected problem detail."
+
+    expected := &problem.Problem{}
+    expected.Status(expectedStatusCode)
+    expected.Detail(expectBodyContains)
+
+    s := mocks.NewArticlesService()
+    s.On(routine, mock.AnythingOfType("*gin.Context"), id).Return(expected)
+
+    engine := gin.Default()
+    engine.POST(target, NewArticlesHandler(s).Hide)
+
+    recorder := httptest.NewRecorder()
+
+    engine.ServeHTTP(recorder, request)
+
+    assert.Equal(t, expectedStatusCode, recorder.Code)
+    assert.Contains(t, recorder.Body.String(), expectBodyContains)
+    assert.Empty(t, recorder.Result().Cookies())
+    assert.Contains(t, recorder.Result().Header.Get("Content-Type"), "application/problem+json")
+  })
+
+  t.Run("unexpected error", func(t *testing.T) {
+    unexpected := errors.New("unexpected error")
+    expectedStatusCode := http.StatusInternalServerError
+    expectBodyContains := "An unexpected error occurred while processing your request"
+
+    s := mocks.NewArticlesService()
+    s.On(routine, mock.AnythingOfType("*gin.Context"), id).Return(unexpected)
+
+    engine := gin.Default()
+    engine.POST(target, NewArticlesHandler(s).Hide)
+
+    recorder := httptest.NewRecorder()
+
+    engine.ServeHTTP(recorder, request)
+
+    assert.Equal(t, expectedStatusCode, recorder.Code)
+    assert.Contains(t, recorder.Body.String(), expectBodyContains)
+    assert.Empty(t, recorder.Result().Cookies())
+    assert.Contains(t, recorder.Result().Header.Get("Content-Type"), "application/problem+json")
+  })
+}
