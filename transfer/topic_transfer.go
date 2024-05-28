18cd180c8851a53f78edc6f087e7a2d3d8d8ba44
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-05-28 14:13:04 -0600

    Add topic transfer types (#17).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

---
+++ transfer/topic_transfer.go
@@ -0,0 +1,13 @@
+package transfer
+
+// TopicCreation represents the data required to create a new topic entry.
+type TopicCreation struct {
+  ID   string
+  Name string `json:"name" binding:"required,max=32"`
+}
+
+// TopicUpdate represents the data required to update an existing topic entry.
+type TopicUpdate struct {
+  ID   string
+  Name string `json:"name" binding:"required,max=32"`
+}
